8f17b617b9d589e77eb92e83d0096d543e7e7a50
absolute-boi <chairman@absoluteboi.com>
2023-10-16 11:10:10 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const { src, dest, watch, series } = require("gulp");
+
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return src("absolute-styles/**/*.scss").pipe(sass()).pipe(dest("css"));
+}
+
+function watchTask() {
+  watch(["absolute-styles/**/*.scss"], buildStyles);
+}
+
+exports.default = series(buildStyles, watchTask);
