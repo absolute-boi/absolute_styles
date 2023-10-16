b6cb4cbb6ddc0aab68bd88370f9d4ee3816dde24
absolute-boi <diegoenriquezserrano@protonmail.com>
2023-10-16 12:10:12 -0500

    rename dir

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("absolute-styles/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("absolute-scss/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["absolute-styles/**/*.scss"], buildStyles);
+  watch(["absolute-scss/**/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
